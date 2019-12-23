16652372927220e8812f5a5d34920c5e1b93aaad
ssairafe <sariaphd@gmail.com>
2019-12-22 20:09:46 -0800

    previous commit was actually bootstrap link this commit is fetch condition and console log removal

--- server/stories.js
+++ server/stories.js
@@ -3,8 +3,7 @@ const connection = require('./connection');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  connection.execute('SELECT * FROM `stories`', (err, rows, fields) => {
-    console.log(rows);
+  connection.execute('SELECT * FROM `stories` WHERE stories.storyCompleted = true', (err, rows, fields) => {
     if (err) return next(err);
     if (rows[0] === undefined) {
       res.send('No stories available');
