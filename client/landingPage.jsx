c68d59313f088a2410c372d2c9ec042b8c553fac
ssairafe <sariaphd@gmail.com>
2019-12-22 20:06:17 -0800

    created basic structure for finished stories and proper data flow

--- client/landingPage.jsx
+++ client/landingPage.jsx
@@ -6,20 +6,48 @@ export default function LandingPage(props) {
 
   useEffect(() => {
     const fetchData = async () => {
-      console.log(setStories);
       const result = await axios(
         '/api/stories'
       );
-      setStories(result.data);
+      await setStories([
+        result.data
+      ]);
     };
+
     fetchData();
-    console.log(finishedStories);
   }, []);
 
+  let finishedStoryElements = finishedStories.map(story => {
+    return (
+      <div key={story.storyID}>
+        <h3>{story.title}</h3>
+        <ul>
+          <li>{story.creator}</li>
+          <li>{story.author1}</li>
+          <li>{story.author2}</li>
+          <li>{story.author3}</li>
+        </ul>
+        <p>{story.part1 + ' ' + story.part2 + ' ' + story.part3 + ' ' + story.part4 + ' '}</p>
+      </div>
+    );
+  });
+
   return (
-    <div>
-      <button onClick={() => { props.changeView('startStory'); }}>Start Story</button>
-      <button onClick={() => { props.changeView('addToStory'); }}>Add to Story</button>
+    <div className="container">
+      <div className="row">
+        <div className="col-3"></div>
+        <div className="col-6" style={{ textAlign: 'center' }}>
+          <button onClick={() => { props.changeView('startStory'); }}>Start Story</button>
+          <button onClick={() => { props.changeView('addToStory'); }}>Add to Story</button>
+        </div>
+        <div className="col-3"></div>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <h1>Finished Stories</h1>
+          {finishedStoryElements}
+        </div>
+      </div>
     </div>
   );
 }
