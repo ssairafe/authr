297373005eb5b9370a47762c3a7250b10ce39f4a
ssairafe <sariaphd@gmail.com>
2020-01-14 14:58:10 -0800

    navbar created and styled

---
+++ client/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav className="myNav navbar navbar-light bg-grey fixed-top" >
+      <div className="logoBox navbar-brand" href="#">
+        <h1 className="logo d-inline-block align-top" alt="">
+          authr
+        </h1>
+      </div>
+    </nav>
+  );
+}
