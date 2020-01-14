d6f02e9d24efb3c3bda9812ec732bce1478b5279
ssairafe <sariaphd@gmail.com>
2020-01-13 19:09:55 -0800

    backend api endpoint to add a new story to database created and functioning properly

--- server/stories.js
+++ server/stories.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const connection = require('./connection');
 const router = express.Router();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
 router.get('/', (req, res, next) => {
   connection.execute('SELECT * FROM `stories` WHERE stories.storyCompleted = true', (err, rows, fields) => {
@@ -9,6 +11,21 @@ router.get('/', (req, res, next) => {
       res.send('No stories available');
     }
     res.json(rows[0]);
+  });
+});
+
+router.post('/', jsonParser, (req, res, next) => {
+  let params = [req.body.newStory.title, req.body.newStory.creator, '', '', '', req.body.newStory.part1, '', '', '', 0];
+  connection.execute('INSERT INTO `stories` (`storyID`, `title`, `creator`, `author1`, `author2`, `author3`, `part1`, `part2`, `part3`, `part4`, `storyCompleted`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', params, (err, rows, fields) => {
+
+    if (err) return next(err);
+    let response = {
+      requestBody: req.body,
+      mySqlRows: rows
+    };
+    if (!err) {
+      res.status(201).json(response);
+    }
   });
 });
 
