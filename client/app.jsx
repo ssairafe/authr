a889ed0907671ff65adfc19570f5e6d871c852d2
ssairafe <sariaphd@gmail.com>
2020-01-20 13:11:16 -0800

    view story component added to app component with state for story to view

--- client/app.jsx
+++ client/app.jsx
@@ -3,12 +3,17 @@ import React, { useState } from 'react';
 import LandingPage from './landingPage';
 import StartStory from './startStory';
 import AddToStory from './addToStory';
+import ViewStory from './viewStory';
 
 export default function App() {
   const [view, setView] = useState('landingPage');
+  const [storyToView, setViewStory] = useState({});
 
+  if (view === 'viewStory') {
+    return <ViewStory story={storyToView} changeView={setView} />;
+  }
   if (view === 'landingPage') {
-    return <LandingPage changeView={setView} />;
+    return <LandingPage setStory={setViewStory} changeView={setView} />;
   }
   if (view === 'startStory') {
     return <StartStory changeView={setView} />;
