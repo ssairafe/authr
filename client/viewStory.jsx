31a81bfce2edc8b25f24b658fcdec59f16e75e60
ssairafe <sariaphd@gmail.com>
2020-01-21 16:42:05 -0800

    new styles for view story

--- client/viewStory.jsx
+++ client/viewStory.jsx
@@ -5,19 +5,21 @@ export default function ViewStory(props) {
   return (
     <>
     <Navbar />
-      <div style={{ height: '130px' }}></div>
+    <div style={{ height: '130px' }}></div>
     <div className="container">
+      <button type="button" className="btn btn-outline-success" style={{ marginBottom: '2%' }} onClick={() => { props.changeView('landingPage'); }}>Home
+      </button>
       <div className="card finishedStoryView">
         <div className="card-body">
-          <h2 className="card-title">{props.story.title}</h2>
-          <p className="card-text"><small className="text-muted">By: {props.story.author1 + ', ' + props.story.author2 + ', ' + props.story.author3 + ', ' + props.story.author4}</small></p>
+          <h2 className="card-title" style={{ textAlign: 'center' }}>{props.story.title}</h2>
+          <p className="card-text" style={{ textAlign: 'center' }}><small className="text-muted">By: {props.story.author1 + ', ' + props.story.author2 + ', ' + props.story.author3 + ', ' + props.story.author4}</small></p>
           <p className="card-text finishedStoryPart ">{props.story.part1}</p>
           <p className="card-text finishedStoryPart ">{props.story.part2}</p>
           <p className="card-text finishedStoryPart ">{props.story.part3}</p>
           <p className="card-text finishedStoryPart ">{props.story.part4}</p>
         </div>
       </div>
-      <button type="button" className="btn btn-outline-success" style={{ marginBottom: '2%' }} onClick={() => { props.changeView('landingPage'); }}>Home</button>
+      <button type="button" className="btn btn-outline-success" style={{ marginBottom: '3%' }} onClick={() => { props.changeView('landingPage'); }}>Home</button>
     </div>
     </>
   );
