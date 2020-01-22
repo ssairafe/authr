0c1881131f6622f142a38f1e7b204e32c3d67765
ssairafe <sariaphd@gmail.com>
2020-01-22 12:04:14 -0800

    fixed node err

--- server/incompleteStories.js
+++ server/incompleteStories.js
@@ -7,9 +7,7 @@ const jsonParser = bodyParser.json();
 router.get('/', jsonParser, (req, res, next) => {
   connection.execute('SELECT * FROM `stories` WHERE stories.storyCompleted = false', (err, rows, fields) => {
     if (err) return next(err);
-    if (rows[0] === undefined) {
-      res.send('No stories available');
-    }
+
     let randomStoryIndex = Math.floor(Math.random() * rows.length);
     res.json((rows[randomStoryIndex]));
   });
