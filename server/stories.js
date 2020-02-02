69b4c33381e2b51917194bc55a5cb1cc0d7319dc
ssairafe <sariaphd@gmail.com>
2020-02-01 17:26:28 -0800

    class stories are completely seperate from general app stories

--- server/stories.js
+++ server/stories.js
@@ -12,18 +12,34 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', jsonParser, (req, res, next) => {
-  let params = [req.body.newStory.title, req.body.newStory.author1, '', '', '', req.body.newStory.part1, '', '', '', 0];
-  connection.execute('INSERT INTO `stories` (`storyID`, `title`, `author1`, `author2`, `author3`, `author4`, `part1`, `part2`, `part3`, `part4`, `storyCompleted`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', params, (err, rows, fields) => {
+  if (req.body.className) {
+    let params = [req.body.classID, req.body.newStory.title, req.body.newStory.author1, '', '', '', req.body.newStory.part1, '', '', '', 0];
+    connection.execute('INSERT INTO `stories` (`storyID`, `class`, `title`, `author1`, `author2`, `author3`, `author4`, `part1`, `part2`, `part3`, `part4`, `storyCompleted`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', params, (err, rows, fields) => {
 
-    if (err) return next(err);
-    let response = {
-      requestBody: req.body,
-      mySqlRows: rows
-    };
-    if (!err) {
-      res.status(201).json(response);
-    }
-  });
+      if (err) return next(err);
+      let response = {
+        requestBody: req.body,
+        mySqlRows: rows
+      };
+      if (!err) {
+        res.status(201).json(response);
+      }
+    });
+  } else {
+    let params = [req.body.newStory.title, req.body.newStory.author1, '', '', '', req.body.newStory.part1, '', '', '', 0];
+    connection.execute('INSERT INTO `stories` (`storyID`, `title`, `author1`, `author2`, `author3`, `author4`, `part1`, `part2`, `part3`, `part4`, `storyCompleted`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', params, (err, rows, fields) => {
+
+      if (err) return next(err);
+      let response = {
+        requestBody: req.body,
+        mySqlRows: rows
+      };
+      if (!err) {
+        res.status(201).json(response);
+      }
+    });
+  }
+
 });
 
 module.exports = router;
