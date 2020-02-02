1685eb3e70ab8cca803da287c6fb79ad9174ed52
ssairafe <sariaphd@gmail.com>
2020-02-01 17:22:25 -0800

    props passed to navbar enter class becomes exit class after entering a class

--- client/navbar.jsx
+++ client/navbar.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
 export default function Navbar(props) {
+  const exitClassFunction = () => {
+    props.setClass('');
+    props.changeView('landingPage');
+  };
+
+  const exitClass = <button type="button" className="btn btn-outline-success align-right" onClick={exitClassFunction}>Exit Class</button>;
+
+  const enterClass = <button type="button" className="btn btn-outline-success align-right" onClick={() => { props.changeView('enterClass'); }}>Enter A Class</button>;
+
   return (
     <nav className="myNav navbar navbar-light bg-grey fixed-top" >
       <div className="logoBox navbar-brand" href="#">
@@ -8,7 +17,9 @@ export default function Navbar(props) {
           authr
         </h1>
       </div>
-      <button type="button" className="btn btn-outline-success align-right" onClick={() => { props.changeView('enterClass'); }}>Enter A Class</button>
+      {
+        props.className ? exitClass : enterClass
+      }
     </nav>
   );
 }
