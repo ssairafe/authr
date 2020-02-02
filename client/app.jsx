d7af808021cf4281e218d0149230b5ab239c4cff
ssairafe <sariaphd@gmail.com>
2020-02-01 17:20:25 -0800

    passed new props down chain added new components

--- client/app.jsx
+++ client/app.jsx
@@ -11,28 +11,32 @@ export default function App() {
   const [view, setView] = useState('landingPage');
   const [storyToView, setViewStory] = useState({});
   const [className, setClass] = useState('');
+  const [classID, setID] = useState(null);
 
   if (view === 'viewStory') {
     return (
     <>
-      <Navbar changeView={setView} />
+        <Navbar changeView={setView} className={className} setClass={setClass} />
       <ViewStory story={storyToView} changeView={setView} />
+      <div style={{ height: '4rem' }}></div>
     </>
     );
   }
   if (view === 'landingPage') {
     return (
     <>
-      <Navbar changeView={setView} />
+        <Navbar changeView={setView} className={className} setClass={setClass}/>
         <LandingPage setStory={setViewStory} changeView={setView} className={className} />
+        <div style={{ height: '4rem' }}></div>
     </>
     );
   }
   if (view === 'startStory') {
     return (
     <>
-      <Navbar changeView={setView} />
-        <StartStory changeView={setView} className={className} />
+        <Navbar changeView={setView} className={className} setClass={setClass} />
+        <StartStory changeView={setView} className={className} classID={classID} />
+        <div style={{ height: '4rem' }}></div>
     </>
     );
   }
@@ -40,16 +44,18 @@ export default function App() {
   if (view === 'addToStory') {
     return (
     <>
-      <Navbar changeView={setView} />
-        <AddToStory changeView={setView} className={className} />
+        <Navbar changeView={setView} className={className} setClass={setClass} />
+        <AddToStory changeView={setView} className={className} classID={classID} />
+        <div style={{ height: '4rem' }}></div>
    </>
     );
   }
   if (view === 'enterClass') {
     return (
       <>
-        <Navbar changeView={setView} />
-        <EnterClass setClass={setClass} changeView={setView} />
+        <Navbar changeView={setView} className={className} setClass={setClass}/>
+        <EnterClass setClass={setClass} changeView={setView} setID={setID} />
+        <div style={{ height: '4rem' }}></div>
       </>
     );
   }
