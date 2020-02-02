b270ed718e13eb4d21f559659ef3fa71888e8806
ssairafe <sariaphd@gmail.com>
2020-02-01 17:24:01 -0800

    removed dead code

--- server/class.js
+++ server/class.js
@@ -5,13 +5,10 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
 router.get('/id', (req, res, next) => {
-  console.log(req.query.className.className);
-  console.log(req.query.className);
-  connection.execute('SELECT * FROM `classes` WHERE classes.className = ?', [req.query.className], (err, rows, fields) => {
-    console.log(rows);
+  let val = JSON.parse(req.query.className);
+  connection.execute('SELECT * FROM `classes` WHERE classes.className = ?', [val.className], (err, rows, fields) => {
     if (err) return next(err);
     if (rows.length === 0) {
-      console.log('No class with this name');
       res.json(rows);
     } else {
       res.json(rows);
@@ -23,16 +20,12 @@ router.get('/', jsonParser, (req, res, next) => {
   connection.execute('SELECT * FROM `classes` WHERE classes.className = ?', [req.query.className], (err, rows, fields) => {
     if (err) return next(err);
     if (rows.length === 0) {
-      console.log('No class with this name');
       res.json(rows);
       return;
     }
     let classId = rows[0].classID;
     connection.execute('SELECT * FROM `stories` WHERE stories.class = ? AND stories.storyCompleted = true', [classId], (err, rows, fields) => {
       if (err) return next(err);
-      if (rows.length === 0) {
-        console.log('No stories in this class');
-      }
       res.json(rows);
     });
   });
